01943097a0112f71a116066c72adf36cf39d8cb5
Edison Orozco <edorozc@bancolombia.com.co>
2022-07-24 20:52:47 -0500

    Se agregar sql scripts

---
+++ DDL_db_acceso_jdbc.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE tbl_autor(
+	autor_id serial primary key,
+	nombre varchar(45),
+	email varchar(45)
+);
+
+CREATE TABLE tbl_libro (
+	libro_id serial primary key,
+	titulo varchar(128),
+	description varchar(512),
+	publicado varchar(100),
+	autor_id int,
+	precio decimal,
+	foreign key (autor_id) 
+	references tbl_autor (autor_id)
+);
